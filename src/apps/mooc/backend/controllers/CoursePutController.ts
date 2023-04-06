a3b0d05235f582ccc20556eb13c12d4aff8da355
Miguel Arreaza <mg.arreaza.13@gmail.com>
2023-04-06 18:35:01 -0400

    feat: add logic v1 to CoursePutController

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -2,9 +2,19 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 import { Controller } from './Controller';
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 
 export class CoursePutController implements Controller {
-	run(req: Request, res: Response): void {
-		res.status(httpStatus.CREATED).send();
-	}
+  constructor(private courseCreator: CourseCreator) {}
+
+  async run(req: Request, res: Response): Promise<void> {
+    const id: string = req.params.id;
+    const name: string = req.body.name;
+    const duration: string = req.body.duration;
+
+    await this.courseCreator.run(id, name, duration);
+
+    res.status(httpStatus.CREATED).send();
+
+  }
 }
