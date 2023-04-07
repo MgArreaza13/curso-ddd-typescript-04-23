1343650cea2a65c3913715408966790b1d2e82be
Miguel Arreaza <mg.arreaza.13@gmail.com>
2023-04-07 16:35:08 -0400

    feat: creating testing with ramdom and faker

--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -4,6 +4,8 @@ import { CourseDuration } from '../../../../../src/Contexts/Mooc/Courses/domain/
 import { CourseName } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseName';
 import { Uuid } from '../../../../../src/Contexts/Shared/domain/value-object/Uuid';
 import { CourseRepositoryMock } from '../__mocks__/CourseRepositoryMock';
+import { CourseMother } from '../domain/CourseMother';
+import { CreateCourseCommandMother } from './create/CreateCourseCommandMother';
 
 let repository: CourseRepositoryMock;
 let creator: CourseCreator;
@@ -50,5 +52,16 @@ describe('CourseCreator', () => {
     }).rejects.toThrow(
       'The Course Name <some-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-namesome-name> has more than 30 characters'
     );
+  });
+
+  it('should create a valid course with CreateCourseRequestMother', async () => {
+    const request = CreateCourseCommandMother.random()
+
+    const course = CourseMother.from(request)
+
+    await creator.run(request);
+
+    repository.assertSaveHaveBeenCalledWith(course);
+    repository.assertLastSavedCourseIs(course);
   });
 });
